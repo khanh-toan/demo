08e40d3bbc9e298f3ac063d8aa8fd00e274ccfb8
khanh-toan <toandoitnow11@gmail.com>
2023-10-08 21:33:26 +0700

    test

--- src/main/java/com/khanhtoan/demo/AssigmentApplication.java
+++ src/main/java/com/khanhtoan/demo/AssigmentApplication.java
@@ -1,7 +1,10 @@
 package com.khanhtoan.demo;
 
+import com.khanhtoan.demo.entity.Student;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AssigmentApplication {
@@ -13,11 +16,22 @@ public class AssigmentApplication {
 	/*@Bean
 	public CommandLineRunner commandLineRunner(TeacherRepository teacherRepository){
 		return runner -> {
-			findTeacherBy(teacherRepository);
+			//findTeacherBy(teacherRepository);
+			findStudentBy(teacherRepository);
 		};
-	}
+	}*/
 
-	private void findTeacherBy(TeacherRepository userRepository) {
+	/*private void findStudentBy(TeacherRepository teacherRepository) {
+		int id = 1;
+		Student s = teacherRepository.findStudentById(id);
+		if(s != null && s.getUser() != null){
+			System.out.println("Tên sinh vien: " + s.getUser().getName());
+		}else{
+			System.out.println("Không tìm thấy sinh viên với id: " +  id);
+		}
+	}*/
+
+	/*private void findTeacherBy(TeacherRepository userRepository) {
 		int teacherId = 1; // Thay đổi id giáo viên tại đây
 		Teacher teacher = userRepository.findById(teacherId);
 		if (teacher != null && teacher.getUser() != null) {
